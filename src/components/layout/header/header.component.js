6d9aed7e3bcc4aefa04e4f153c5f70dddeb5cf57
linklib <linklib.web@yandex.ru>
2024-05-12 13:08:11 +0300

    Слияни ес исходником-2

--- src/components/layout/header/header.component.js
+++ src/components/layout/header/header.component.js
@@ -1,24 +1,35 @@
-import ChildComponent from "@/core/component/child.component"
-import renderService from "@/core/services/render.service"
-import { UserItem } from "@/components/ui/user-item/user-item.component"
-import { $R } from "@/core/rquery/rquery.lib"
-import { Heading } from "@/components/ui/heading/heading.component"
+import ChildComponent from '@/core/component/child.component'
+import renderService from '@/core/services/render.service'
+import { UserItem } from '@/components/ui/user-item/user-item.component'
+import { Logo } from './logo/logo.component'
+import { LogoutButton } from './logout-button/logout-button.component'
+import { Search } from './search/search.component'
 
-import template from "./header.template.html"
 import styles from './header.module.scss'
+import template from './header.template.html'
 
-export class Header extends ChildComponent {   
-   
-       
-    render(){
-        this.element = renderService.htmlToElement(template, [], styles)
+export class Header extends ChildComponent {
+	constructor({router}){
+		super()
 
-        $R(this.element).append(new UserItem().render())
+		this.component = 'header'
+		this.router = router
+		
+	}
+	render() {
+		this.element = renderService.htmlToElement(template, [
+			Logo,
+			new LogoutButton({
+				router: this.router
+			}),			
+			Search,			
+			new UserItem({
+				avatarPath:
+					'https://png.pngtree.com/thumb_back/fw800/background/20230610/pngtree-picture-of-a-blue-bird-on-a-black-background-image_2937385.jpg',
+				name: 'Dima'
+	})
+		], styles)
 
-        //$R(this.element).append(new Heading().render()).text('Это заголовок')
-        const heading = $R(this.element).append(new Heading().render()).find('h3').text('Это заголовок')
-       
-        return this.element
-        
-    }
+		return this.element
+	}
 }
